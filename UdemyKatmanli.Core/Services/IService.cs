abb70f647a0d5f1a6f7ff02951fda6c9b3fd5dac
UfukDusovali <ufuk.dusovali@gmail.com>
2023-12-22 14:39:39 +0300

    karısık

--- UdemyKatmanli.Core/Services/IService.cs
+++ UdemyKatmanli.Core/Services/IService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using UdemyKatmanli.Core.Repositories;
 
 namespace UdemyKatmanli.Core.Services;
 
