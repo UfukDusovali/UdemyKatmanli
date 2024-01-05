038217e2f93c4be0b37b389fd76a00fa5020b864
UfukDusovali <ufuk.dusovali@gmail.com>
2024-01-05 14:13:49 +0300

    ocak5 guncelleme

--- UdemyKatmanli.API/obj/Debug/net8.0/UdemyKatmanli.API.AssemblyInfo.cs
+++ UdemyKatmanli.API/obj/Debug/net8.0/UdemyKatmanli.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UdemyKatmanli.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4815d7d4cda95cab053a4500cd420180664c07e")]
 [assembly: System.Reflection.AssemblyProductAttribute("UdemyKatmanli.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UdemyKatmanli.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- UdemyKatmanli.Service/obj/Debug/net8.0/UdemyKatmanli.Service.AssemblyInfo.cs
+++ UdemyKatmanli.Service/obj/Debug/net8.0/UdemyKatmanli.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UdemyKatmanli.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4815d7d4cda95cab053a4500cd420180664c07e")]
 [assembly: System.Reflection.AssemblyProductAttribute("UdemyKatmanli.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UdemyKatmanli.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
